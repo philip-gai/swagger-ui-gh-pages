7b07bf9da83920f5d1bd904199ee6a28adeee840
Philip Gai <philip-gai@github.com>
2022-02-01 14:24:48 -0600

    Implement simple swagger-ui app

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Demo of Swagger UI
+
+- Documentation: [swagger-api/swagger-ui](https://github.com/swagger-api/swagger-ui)
+- Demo project: [webpack-getting-started](https://github.com/swagger-api/swagger-ui/tree/master/docs/samples/webpack-getting-started)
+
+## Usage
+
+```npm
+npm install
+npm run build
+npm start
+```
+
+## Contributing
+
+To update the swagger docs, copy your OpenApi spec files into the [`src/specs`](./src/specs) folder.
---
+++ scripts/Update-SwaggerConfig.ps1
@@ -0,0 +1,49 @@
+<#
+  .SYNOPSIS
+  Updates the swagger-config.json file with the urls from the specs folder.
+#>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$false)]
+    [String]$RepoFolder = "swagger-ui-simple/",
+
+    [Parameter(Mandatory=$false)]
+    [String]$SpecFolder = "./src/specs/",
+
+    [Parameter(Mandatory=$false)]
+    [String]$OutFile = "./src/swagger-config.json"
+)
+
+$ErrorActionPreference = "Stop"
+
+if (-not (Get-Module -ListAvailable -Name powershell-yaml)) {
+    Install-Module powershell-yaml -Force
+}
+Import-Module powershell-yaml
+
+$files = Get-ChildItem $SpecFolder -Recurse -File | Select-Object FullName, Extension
+
+$swaggerConfig = @{}
+$swaggerConfig.urls = @()
+
+foreach ($file in $files) {
+    $url = $file.FullName.split($RepoFolder)[1]
+    if ($file.Extension -eq ".json") {
+        $spec = Get-Content $file.FullName | ConvertFrom-Json
+    } elseif ($file.Extension -eq ".yaml" -or $file.Extension -eq ".yml") {
+        $spec = Get-Content $file.FullName | ConvertFrom-Yaml
+    } else {
+        Write-Error "Unsupported file type: $($file.Extension)"
+    }
+    $name = $spec.info.title
+    $swaggerConfig.urls += [PSCustomObject]@{
+        url = $url
+        name = $name
+    }
+}
+
+[array]$swaggerConfig.urls = $swaggerConfig.urls | Sort-Object -Property url | Sort-Object -Property name -Unique
+Write-Host $swaggerConfig.urls
+
+$swaggerConfig | ConvertTo-Json | Out-File $OutFile -Encoding utf8 -Force
